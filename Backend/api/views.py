03ffcdecf17a2333ed8b9a8f893146fbd47225f0
Aishwarya Manoharan <143560747+aishwarya-mano@users.noreply.github.com>
2023-09-18 20:07:10 +0000

    Added login url

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -8,3 +8,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the API index.")
+
+def login(request):
+    return HttpResponse("Hello, world. You're logeed in.")
