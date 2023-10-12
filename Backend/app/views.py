1650f9c85549a7140d9f52817a878e41533b0bd1
Aishwarya Manoharan <143560747+aishwarya-mano@users.noreply.github.com>
2023-10-12 11:27:53 -0700

    Update views.py

--- Backend/app/views.py
+++ Backend/app/views.py
@@ -1,16 +1,17 @@
 from app import app, db
-from flask import request, jsonify
+from flask import request, jsonify, session
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from app.db_handler import Users, Files
 from app import utils
 from datetime import datetime
 from passlib.apps import custom_app_context as pwd_context
 
+
 import pytz
 import re
 
 pt_timezone = pytz.timezone('US/Pacific')
-S3_BUCKET = "YOUR_S3_BUCKET_NAME"
+S3_BUCKET = "YOUR_BUCKT_NAME"
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -38,14 +39,18 @@ def login():
         _password = req['password']
 
         user = Users.query.filter_by(email_id=_email_id).first()
-        if user and pwd_context.verify(_password, user.password):
+        if user is None:
+            status = False
+            return jsonify({'message': 'Email ID not registered!', 'code': '201', 'success': status})            
+        if pwd_context.verify(_password, user.password):
             login_user(user)
             status = True
-            print(current_user)
-            return jsonify({'message': 'Login Successful', 'code': '200', 'success': status, 'user': current_user.first_name})
+            session["id"] = _email_id
+            is_admin = (user.user_role == 'admin')
+            return jsonify({'message': 'Login Successful!', 'code': '200', 'success': status, 'user': current_user.first_name, 'is_admin': is_admin})
         else:
             status = False
-            return jsonify({'message': 'Enter Valid Username/Password', 'code': '201', 'success': status})
+            return jsonify({'message': 'Entered wrong Password!', 'code': '201', 'success': status})
 
 
 @app.route("/logout", methods=['POST'])
@@ -68,6 +73,7 @@ def register_user():
         existing_user = Users.query.filter_by(email_id=email).first()
 
         if existing_user:
+            print(existing_user)
             status = False
             return jsonify({'message': 'User already exists', 'code': '201', 'success': status})
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -88,14 +94,14 @@ def register_user():
 def upload_file():
     if request.method != 'POST':
         return
+    file = request.files.get('file', None)
+    _desc = request.form.get('description', '')
 
-    file = request.files['file']
-    _desc = request.form['description']
     _current_user_id = current_user.user_id 
     status = False
 
     if file is None or file.filename == '':
-        return jsonify({'message': 'No file selected', 'code': '201', 'sucess': status})
+        return jsonify({'message': 'No file selected', 'code': '201', 'success': status})
 
     if file.content_length > app.config['MAX_CONTENT_LENGTH']:
         return jsonify({'message': 'File Content exceeded the Max allowed', 'code': '201', 'success': status})
@@ -108,7 +114,7 @@ def upload_file():
         file_url = utils.upload_file_to_aws(file)
     except Exception as ex:
         msg = "Error in uploading file to S3: {}".format(ex)
-        return jsonify({'message': msg, 'code': '201', 'sucess': status})
+        return jsonify({'message': msg, 'code': '201', 'success': status})
 
     _filename = utils.secure_filename(file.filename)
     new_file = Files(_filename, _desc, file_url,
@@ -121,25 +127,51 @@ def upload_file():
     except Exception as ex:
         return jsonify({'success': False, 'message': str(ex)})
 
+@app.route('/view_file/<int:file_id>', methods=['GET'])
+@login_required
+def view_file(file_id):
+    current_user_id = current_user.user_id
+    file = Files.query.filter_by(user_id=current_user_id, file_id = file_id).first()
+    if file is None:
+        return jsonify({'file': {}})
+    file_cdn_url =  "{}/{}".format(app.config['CLOUDFRONT_URL'], file.name)
+    file_info = {
+        'file_id': file.file_id,
+        'filename': file.name,
+        'description': file.description,
+        'file_url': file_cdn_url,
+        'creation_time': file.creation_time.strftime('%Y-%m-%d %H:%M:%S'),
+        'updated_time': file.updated_time.strftime('%Y-%m-%d %H:%M:%S')
+    }
+    return jsonify({'file': file_info})
+
 
 @app.route('/view_files', methods=['GET'])
 @login_required
 def view_all_files():
     is_admin = current_user.user_role == 'admin'
     current_user_id = current_user.user_id
+    print("Current User: ", current_user_id)
 
     if is_admin:
-        all_files = Files.query.all()
+        # all_files = Files.query.join(Users).all()
+        all_files = db.session.query(Files, Users).join(Users).all()
     else:
-        all_files = Files.query.filter_by(user_id=current_user_id)
+        all_files = db.session.query(Files, Users).join(Users).filter_by(user_id=current_user_id).all()
+    
+    print(all_files);
+    
 
     files_data = []
-    for file in all_files:
+    for file, user in all_files:
+        print(file);
+        file_cdn_url =  "{}/{}".format(app.config['CLOUDFRONT_URL'], file.name)
         file_info = {
             'file_id': file.file_id,
             'filename': file.name,
             'description': file.description,
-            'file_url': file.url,
+            'file_url': file_cdn_url,
+            'uploaded_by': user.last_name + ', ' + user.first_name,
             'creation_time': file.creation_time.strftime('%Y-%m-%d %H:%M:%S'),
             'updated_time': file.updated_time.strftime('%Y-%m-%d %H:%M:%S')
         }
@@ -152,10 +184,9 @@ def view_all_files():
 def update_file():
     if request.method != 'POST':
         return
-
-    updated_file = request.files['file']
-    file_id = request.form['file_id']
-    desc = request.form['description']
+    updated_file = request.files.get('file', None)
+    desc = request.form.get('description', '')
+    file_id = request.form.get('file_id', '')
     current_user_id = current_user.user_id
     status = False
 
@@ -163,6 +194,22 @@ def update_file():
     if file is None:
         return jsonify({'message': "File ID Not Found", 'code': '201', 'status': status})
 
+    if desc == file.description and updated_file is None:
+        return jsonify({'message': "Nothing to Update", 'code': '201', 'status': True})
+    print(desc);
+    print(file.description);
+    if desc != file.description and updated_file is None:
+        # only change in desc
+        try:
+            file.description = desc
+            file.updated_time = datetime.now(pt_timezone)
+            db.session.commit()
+            status = True
+            return jsonify({'message': 'Description updated successfully', 'code': '200', 'success': status})
+        except Exception as ex:
+            return jsonify({'success': False, 'message': str(ex)})        
+
+    # below code file is updated here
     file_url = file.url
     file_name = file.name
 
