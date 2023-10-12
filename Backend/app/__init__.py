67072e56c9ae6d3be0e0bb917ce674fcf0a0190c
Aishwarya Manoharan <143560747+aishwarya-mano@users.noreply.github.com>
2023-10-12 11:29:31 -0700

    Update __init__.py
    Configuration updates

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 from flask_login import LoginManager
+from flask_session import Session
 
 
 print('Version: ',sys.version)
@@ -12,8 +13,13 @@ CORS(app,supports_credentials=True)
 app.secret_key = 'YOUR_APP_SECRET_KEY'
 app.config.from_object('config')
 
+#SESSION
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # AWS RDS
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:PROJECT_NAME.XXXXXXXX.us-east-1.rds.amazonaws.com/fileservice'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:PROJECT_NAME.XXXXXXXX.us-east-1.rds.amazonaws.com/xxxxxxx'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 
 
@@ -23,6 +29,7 @@ app.config['S3_BUCKET'] = S3_BUCKET
 app.config['S3_KEY'] = "S3_KEY"
 app.config['S3_SECRET'] = "XXX/S3_SECRET_KEY_VALUE"
 app.config['S3_LOCATION'] = 'http://YOUR_S3_BUCKET_NAME.s3.amazonaws.com/'.format(S3_BUCKET)
+app.config['CLOUDFRONT_URL'] = 'CLOUDFRONT_URL'
 
 
 db = SQLAlchemy(app)
